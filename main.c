ce90d3bd5f5e511fde6c5c87ab625855f1cfb8f0
Hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-01-23 19:20:48 +0100

    this is just a trial

---
+++ main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(void){
+
+    int i=1;
+    //i= rows
+
+    for (i=1; i<=5; i++)
+    {
+        int j=1;
+        // j= colums
+        
+        for (j=1; j<=i; j++)
+
+        printf("*");
+        printf("\n");
+    }
+
+    return(0);
+
+
+
+
+
+}
